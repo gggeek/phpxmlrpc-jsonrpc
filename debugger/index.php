4e0c5f37d09ad2a50df5b29cfbfab42938a6c9b3
gggeek <giunta.gaetano@gmail.com>
2021-01-05 15:01:25 +0000

    add debugger favicon

--- debugger/index.php
+++ debugger/index.php
@@ -6,4 +6,6 @@ if (!file_exists(__DIR__.'/../vendor/autoload.php') || !file_exists(__DIR__.'/..
 
 include_once(__DIR__.'/../vendor/autoload.php');
 
+define('DEFAULT_WSTYPE', '1');
+
 include(__DIR__.'/../vendor/phpxmlrpc/phpxmlrpc/debugger/index.php');
