e4e158c89561f90efc57a3be5253675a225525ed
gggeek <giunta.gaetano@gmail.com>
2021-01-13 23:02:38 +0000

    improve debugger autoloading

--- debugger/controller.php
+++ debugger/controller.php
@@ -1,11 +1,15 @@
 <?php
 
-/// @todo support the case of this lib having been installed as dependency, too
-if (!file_exists(__DIR__.'/../vendor/autoload.php') || !file_exists(__DIR__.'/../vendor/phpxmlrpc/phpxmlrpc/debugger/controller.php'))
+$vendorDir = __DIR__.'/../vendor';
+if (!is_dir($vendorDir)) {
+    $vendorDir = __DIR__.'/../../../../vendor';
+}
+
+if (!file_exists($vendorDir.'/autoload.php') || !file_exists($vendorDir.'/phpxmlrpc/phpxmlrpc/debugger/controller.php'))
     die('Please install the dependencies using composer');
 
-include_once(__DIR__.'/../vendor/autoload.php');
+include_once($vendorDir.'/autoload.php');
 
 define('DEFAULT_WSTYPE', '1');
 
-include(__DIR__.'/../vendor/phpxmlrpc/phpxmlrpc/debugger/controller.php');
+include($vendorDir.'/phpxmlrpc/phpxmlrpc/debugger/controller.php');
