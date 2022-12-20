5b2091446f07f8cb57d88ad3bc74c5583744dce5
gggeek <giunta.gaetano@gmail.com>
2022-12-20 17:03:37 +0000

    oops version nr

--- src/PhpJsonRpc.php
+++ src/PhpJsonRpc.php
@@ -5,7 +5,7 @@ namespace PhpXmlRpc\JsonRpc;
 class PhpJsonRpc
 {
     public static $jsonrpcName = "JSON-RPC for PHP";
-    public static $jsonrpcVersion = "1.0.0-dev";
+    public static $jsonrpcVersion = "1.0.0-beta1";
 
     public static $json_decode_flags = 0;
     public static $json_decode_depth = 512;
