43319696bd6ff72f6de18c6b43f9058374942b23
gggeek <giunta.gaetano@gmail.com>
2025-10-03 23:49:34 +0000

    bring in (some of) the upstream changes to benchmark.php

--- extras/benchmark.php
+++ extras/benchmark.php
@@ -1,10 +1,14 @@
 <?php
 /**
- * Benchmarking suite
+ * Benchmarking suite for the PHPJSONRPC lib
  *
  * @author Gaetano Giunta
- * @copyright (c) 2006-2025 G. Giunta
+ * @copyright (C) 2006-2025 G. Giunta
  * @license code licensed under the BSD License: see license.txt
+ *
+ * @todo add a check for response ok in call testing
+ * @todo add support for --help option to give users the list of supported parameters
+ * @todo make number of test iterations flexible
  **/
 
 use PhpXmlRpc\JsonRpc\Client;
@@ -29,24 +33,25 @@ $args = JsonrpcArgParser::getArgs();
 function begin_test($test_name, $test_case)
 {
     global $test_results;
-    if (!isset($test_results[$test_name]))
+    if (!isset($test_results[$test_name])) {
         $test_results[$test_name] = array();
+    }
     $test_results[$test_name][$test_case] = array();
-    list($micro, $sec) = explode(' ', microtime());
-    $test_results[$test_name][$test_case]['time'] = $sec + $micro;
+    $test_results[$test_name][$test_case]['time'] = microtime(true);
 }
 
 function end_test($test_name, $test_case, $test_result)
 {
     global $test_results;
-    list($micro, $sec) = explode(' ', microtime());
-    if (!isset($test_results[$test_name][$test_case]))
+    $end = microtime(true);
+    if (!isset($test_results[$test_name][$test_case])) {
         trigger_error('ending test that was not started');
-    $test_results[$test_name][$test_case]['time'] = $sec + $micro - $test_results[$test_name][$test_case]['time'];
+    }
+    $test_results[$test_name][$test_case]['time'] = $end - $test_results[$test_name][$test_case]['time'];
     $test_results[$test_name][$test_case]['result'] = $test_result;
-
     echo '.';
     flush();
+    @ob_flush();
 }
 
 // Set up PHP structures to be used in many tests
@@ -56,41 +61,49 @@ $data2 = array('zero' => $data1, 'one' => $data1, 'two' => $data1, 'three' => $d
 $data = array($data2, $data2, $data2, $data2, $data2, $data2, $data2, $data2, $data2, $data2);
 $keys = array('zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine');
 
+// Begin execution
+
 $test_results = array();
+$is_web = isset($_SERVER['REQUEST_METHOD']);
 $xd = extension_loaded('xdebug') && ini_get('xdebug.profiler_enable');
-if ($xd)
+if ($xd) {
     $num_tests = 1;
-else
+} else {
     $num_tests = 10;
+}
 
 $test_http = false; // enable/disable tests made over http
 
 $title = 'JSON-RPC Benchmark Tests';
 
-if (isset($_SERVER['REQUEST_METHOD'])) {
+if ($is_web) {
     echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n<html xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"en\" xml:lang=\"en\">\n<head>\n<title>$title</title>\n</head>\n<body>\n<h1>$title</h1>\n<pre>\n";
-    if ($xd) echo "<h4>XDEBUG profiling enabled: skipping remote tests. Trace file is: " . htmlspecialchars(xdebug_get_profiler_filename()) . "</h4>\n";
-    flush();
-    if ($xd) echo "XDEBUG profiling enabled: skipping remote tests\nTrace file is: " . xdebug_get_profiler_filename() . "\n";
 } else {
     echo "$title\n\n";
 }
 
-if (isset($_SERVER['REQUEST_METHOD'])) {
+if ($is_web) {
     echo "<h3>Using lib version: " . PhpJsonRpc::$jsonrpcVersion . " on PHP version: " . phpversion() . "</h3>\n";
+    if ($xd) {
+        echo "<h4>XDEBUG profiling enabled: skipping remote tests. Trace file is: " . htmlspecialchars(xdebug_get_profiler_filename()) . "</h4>\n";
+    }
     flush();
+    ob_flush();
 } else {
     echo "Using lib version: " . PhpJsonRpc::$jsonrpcVersion . " on PHP version: " . phpversion() . "\n";
+    if ($xd)  {
+        echo "XDEBUG profiling enabled: skipping remote tests\nTrace file is: " . xdebug_get_profiler_filename() . "\n";
+    }
 }
 
-$encoder = new Encoder();
+// \PhpXmlRpc\JsonRpc\PhpJsonRpc::$json_decode_depth = 10240;
 
 // test 'old style' data encoding vs. 'automatic style' encoding
 begin_test('Data encoding (large array)', 'manual encoding');
 for ($i = 0; $i < $num_tests; $i++) {
     $vals = array();
     for ($j = 0; $j < 10; $j++) {
-        $valarray = array();
+        $valArray = array();
         foreach ($data[$j] as $key => $val) {
             $values = array();
             $values[] = new Value($val[0], 'int');
@@ -103,9 +116,9 @@ for ($i = 0; $i < $num_tests; $i++) {
             $values[] = new Value($val[7], 'string');
             $values[] = new Value($val[8], 'boolean');
             $values[] = new Value($val[9], 'null');
-            $valarray[$key] = new Value($values, 'array');
+            $valArray[$key] = new Value($values, 'array');
         }
-        $vals[] = new Value($valarray, 'struct');
+        $vals[] = new Value($valArray, 'struct');
     }
     $value = new Value($vals, 'array');
     $out = $value->serialize();
@@ -113,6 +126,7 @@ for ($i = 0; $i < $num_tests; $i++) {
 end_test('Data encoding (large array)', 'manual encoding', $out);
 
 begin_test('Data encoding (large array)', 'automatic encoding');
+$encoder = new Encoder();
 for ($i = 0; $i < $num_tests; $i++) {
     $value = $encoder->encode($data);
     $out = $value->serialize();
@@ -125,27 +139,24 @@ for ($i = 0; $i < $num_tests; $i++) {
 }
 end_test('Data encoding (large array)', 'native encoding', $out);
 
-
 // test 'old style' data decoding vs. 'automatic style' decoding
 $dummy = new Request('');
-$out = new Response($value);
+$out = new Response($data);
 $in = $out->serialize();
 
 begin_test('Data decoding (large array)', 'manual decoding');
 for ($i = 0; $i < $num_tests; $i++) {
-    $response = $dummy->ParseResponse($in, true);
+    $response = $dummy->parseResponse($in, true);
     $value = $response->value();
     $result = array();
-    for ($k = 0; $k < count($value); $k++) {
-        $val1 = $value[$k];
+    foreach($value as $val1) {
         $out = array();
         foreach ($val1 as $name => $val) {
             $out[$name] = array();
-            for ($j = 0; $j < count($val); $j++) {
-                $data = $val[$j];
+            foreach($val as $data) {
                 $out[$name][] = $data->scalarVal();
             }
-        } // while
+        }
         $result[] = $out;
     }
 }
@@ -153,7 +164,7 @@ end_test('Data decoding (large array)', 'manual decoding', $result);
 
 begin_test('Data decoding (large array)', 'automatic decoding');
 for ($i = 0; $i < $num_tests; $i++) {
-    $response = $dummy->ParseResponse($in, true, 'phpvals');
+    $response = $dummy->parseResponse($in, true, 'phpvals');
     $value = $response->value();
 }
 end_test('Data decoding (large array)', 'automatic decoding', $value);
@@ -161,29 +172,34 @@ end_test('Data decoding (large array)', 'automatic decoding', $value);
 begin_test('Data decoding (large array)', 'native decoding');
 for ($i = 0; $i < $num_tests; $i++) {
     // to be fair to phpjsonrpc, we add http checks to every lib
-    $response = $dummy->ParseResponse($in, true, 'json');
+    $response = $dummy->parseResponse($in, true, 'json');
     $value = json_decode($response->value(), true);
     // we do NO error checking here: bound to be faster...
     $value = $value['result'];
 }
 end_test('Data decoding (large array)', 'native decoding', $value);
 
-
+/// @todo add all test permutations found in the phpxmlrpc benchmark
 
 if ($test_http && !$xd) {
 
+    $num_tests = 25;
+
     /// test many calls vs. keep-alives
+
     $value = $encoder->encode($data1);
-    $msg = new Request('interopEchoTests.echoValue', array($value));
-    $msgs = array();
-    for ($i = 0; $i < 25; $i++)
-        $msgs[] = $msg;
+    $req = new Request('interopEchoTests.echoValue', array($value));
+    $reqs = array();
+    for ($i = 0; $i < $num_tests; $i++)
+        $reqs[] = $req;
+
     $server = explode(':', $args['HTTPSERVER']);
     if (count($server) > 1) {
         $c = new Client($args['HTTPURI'], $server[0], $server[1]);
     } else {
         $c = new Client($args['HTTPURI'], $args['HTTPSERVER']);
     }
+
     // do not interfere with http compression
     $c->setOption(Client::OPT_ACCEPTED_COMPRESSION, array());
     //$c->debug=true;
@@ -191,7 +207,7 @@ if ($test_http && !$xd) {
     begin_test('Repeated send (small array)', 'http 10');
     $response = array();
     for ($i = 0; $i < 25; $i++) {
-        $resp = $c->send($msg);
+        $resp = $c->send($req);
         $response[] = $resp->value();
     }
     end_test('Repeated send (small array)', 'http 10', $response);
@@ -200,7 +216,7 @@ if ($test_http && !$xd) {
         begin_test('Repeated send (small array)', 'http 11 w. keep-alive');
         $response = array();
         for ($i = 0; $i < 25; $i++) {
-            $resp = $c->send($msg, 10, 'http11');
+            $resp = $c->send($req, 10, 'http11');
             $response[] = $resp->value();
         }
         end_test('Repeated send (small array)', 'http 11 w. keep-alive', $response);
@@ -209,14 +225,14 @@ if ($test_http && !$xd) {
         begin_test('Repeated send (small array)', 'http 11');
         $response = array();
         for ($i = 0; $i < 25; $i++) {
-            $resp = $c->send($msg, 10, 'http11');
+            $resp = $c->send($req, 10, 'http11');
             $response[] = $resp->value();
         }
         end_test('Repeated send (small array)', 'http 11', $response);
     }
 
     /*begin_test('Repeated send (small array)', 'multicall');
-    $response = $c->send($msgs);
+    $response = $c->send($reqs);
     end_test('Repeated send (small array)', 'multicall', $response);*/
 
     if (function_exists('gzinflate')) {
@@ -226,7 +242,7 @@ if ($test_http && !$xd) {
         begin_test('Repeated send (small array)', 'http 10 w. compression');
         $response = array();
         for ($i = 0; $i < 25; $i++) {
-            $resp = $c->send($msg);
+            $resp = $c->send($req);
             $response[] = $resp->value();
         }
         end_test('Repeated send (small array)', 'http 10 w. compression', $response);
@@ -243,6 +259,6 @@ foreach ($test_results as $test => $results) {
 }
 
 
-if (isset($_SERVER['REQUEST_METHOD'])) {
+if ($is_web) {
     echo "\n</pre>\n</body>\n</html>\n";
 }
