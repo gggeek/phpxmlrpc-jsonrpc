3bc817359fe276ad5a72acb7cfb0310f429d36a2
gggeek <giunta.gaetano@gmail.com>
2025-10-09 17:32:05 +0000

    allow the Wrapper to force sending 1.0/2.0 requests when wrapping remote methods

--- src/Wrapper.php
+++ src/Wrapper.php
@@ -2,9 +2,39 @@
 
 namespace PhpXmlRpc\JsonRpc;
 
+use PhpXmlRpc\JsonRpc\Traits\JsonRpcVersionAware;
 use PhpXmlRpc\Wrapper as BaseWrapper;
 
 class Wrapper extends BaseWrapper
 {
+    use JsonRpcVersionAware;
+
     static protected $namespace = '\\PhpXmlRpc\\JsonRpc\\';
+
+    protected function buildClientWrapperCode($client, $verbatimClientCopy, $prefix = 'xmlrpc', $namespace = '\\PhpXmlRpc\\')
+    {
+        $code = parent::buildClientWrapperCode($client, $verbatimClientCopy, $prefix, $namespace);
+
+        if ($this->jsonrpc_version !== null) {
+            $code .= "\$client->setOption(\PhpXmlRpc\JsonRpc\Client::OPT_JSONRPC_VERSION, '" .
+                str_replace("'", "\'", $this->jsonrpc_version) . "');\n";
+        }
+
+        return $code;
+    }
+
+    /**
+     * @param Client $client
+     * @return Client
+     */
+    protected function cloneClientForClosure($client)
+    {
+        $client = parent::cloneClientForClosure($client);
+
+        if ($this->jsonrpc_version !== null) {
+            $client->setOption(Client::OPT_JSONRPC_VERSION, $this->jsonrpc_version);
+        }
+
+        return $client;
+    }
 }
